03f86dcfceb992f76c1515964c932958cdf82306
Tales <taleskodama@gmail.com>
2025-02-15 23:56:04 -0300

    integrate Firebase authentication and add environment configuration

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase:{
+        apiKey: "AIzaSyB4f61r7ZIyQpKbMk3Eg5ACz3XJII_MyM0",
+        authDomain: "asimovies-85667.firebaseapp.com",
+        projectId: "asimovies-85667",
+        storageBucket: "asimovies-85667.firebasestorage.app",
+        messagingSenderId: "538540895372",
+        appId: "1:538540895372:web:6a81aadc3a3917094e9fa9",
+        measurementId: "G-97JB3GRJT1"
+            }
+}
