20178626dd5e54b17d7e9accc8cf91153567cbec
Tales <taleskodama@gmail.com>
2025-02-15 23:25:41 -0300

    add login component with routing and basic template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<router-outlet></router-outlet>',
   standalone: false,
-  styleUrl: './app.component.scss'
+ 
 })
 export class AppComponent {
   title = 'asimovies';
